0b320ac9ef08835565553264ebc3a83cf8fb6276
Alisa <91307985+bouffee@users.noreply.github.com>
2023-05-21 15:01:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,156 +1,109 @@
-# Python code to implement Conway's Game Of Life
-import argparse
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+"""
+    Pygame of life module. Contains the short engine
+    to simluate the grid of life.
+"""
 
-# setting up the values for the grid
-ON = 255
-OFF = 0
-vals = [ON, OFF]
+import sys
+import time
+from collections import defaultdict
+from copy import deepcopy
 
-def randomGrid(N):
+import pygame
 
-	"""returns a grid of NxN random values"""
-	return np.random.choice(vals, N*N, p=[0.2, 0.8]).reshape(N, N)
+from example_grids import GOSPER_GLIDER
+from grid_defs import Grid, Neighbours
 
-def addGlider(i, j, grid):
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 400
+GRID_COLOR = (0,104,90)
+BACKGROUND_COLOR = (0, 160, 138)
+CELL_COLOR = (246,0,24)
 
-	"""adds a glider with top left cell at (i, j)"""
-	glider = np.array([[0, 0, 255],
-					[255, 0, 255],
-					[0, 255, 255]])
-	grid[i:i+3, j:j+3] = glider
+def get_neighbours(grid: Grid, x: int, y: int) -> Neighbours:
+    """
+        Gets the neighbour states for a particular cell in
+        (x, y) on the grid.
+    """
+    offsets = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
+    possible_neighbours = {(x + x_add, y + y_add) for x_add, y_add in offsets}
+    alive = {(pos[0], pos[1]) for pos in possible_neighbours if pos in grid.cells}
+    return Neighbours(alive, possible_neighbours - alive)
 
-def addGosperGliderGun(i, j, grid):
 
-	"""adds a Gosper Glider Gun with top left
-	cell at (i, j)"""
-	gun = np.zeros(11*38).reshape(11, 38)
+def update_grid(grid: Grid) -> Grid:
+    """
+        Given a grid, this function returns the next iteration
+        of the game of life.
+    """
+    new_cells = deepcopy(grid.cells)
+    undead = defaultdict(int)
 
-	gun[5][1] = gun[5][2] = 255
-	gun[6][1] = gun[6][2] = 255
+    for x, y in grid.cells:
+        alive_neighbours, dead_neighbours = get_neighbours(grid, x, y)
+        if len(alive_neighbours) not in [2, 3]:
+            new_cells.remove((x, y))
 
-	gun[3][13] = gun[3][14] = 255
-	gun[4][12] = gun[4][16] = 255
-	gun[5][11] = gun[5][17] = 255
-	gun[6][11] = gun[6][15] = gun[6][17] = gun[6][18] = 255
-	gun[7][11] = gun[7][17] = 255
-	gun[8][12] = gun[8][16] = 255
-	gun[9][13] = gun[9][14] = 255
+        for pos in dead_neighbours:
+            undead[pos] += 1
 
-	gun[1][25] = 255
-	gun[2][23] = gun[2][25] = 255
-	gun[3][21] = gun[3][22] = 255
-	gun[4][21] = gun[4][22] = 255
-	gun[5][21] = gun[5][22] = 255
-	gun[6][23] = gun[6][25] = 255
-	gun[7][25] = 255
+    for pos, _ in filter(lambda elem: elem[1] == 3, undead.items()):
+        new_cells.add((pos[0], pos[1]))
 
-	gun[3][35] = gun[3][36] = 255
-	gun[4][35] = gun[4][36] = 255
+    return Grid(grid.dim, new_cells)
 
-	grid[i:i+11, j:j+38] = gun
 
-def update(frameNum, img, grid, N):
+def draw_grid(screen: pygame.Surface, grid: Grid) -> None:
+    """
+        This function draws the game of life on the given
+        pygame.Surface object.
+    """
+    # cell_width = screen.get_width() / grid.dim.width
+    # cell_height = screen.get_height() / grid.dim.height
+    cell_width = 10 
+    cell_height = 10
+    border_size = 0
 
-	# copy grid since we require 8 neighbors
-	# for calculation and we go line by line
-	newGrid = grid.copy()
-	for i in range(N):
-		for j in range(N):
+    for x, y in grid.cells:
+        pygame.draw.rect(
+            screen,
+            CELL_COLOR,
+            (
+                x * cell_width + border_size,
+                y * cell_height + border_size,
+                cell_width - border_size,
+                cell_height - border_size,
+            ),
+        )
+    
+def make_squares (surface:pygame.Surface):
+    blockSize = 10 #Set the size of the grid block
+    for x in range(0, WINDOW_WIDTH, blockSize):
+        for y in range(0, WINDOW_HEIGHT, blockSize):
+            rect = pygame.Rect(x, y, blockSize, blockSize)
+            shape_surf = pygame.Surface(rect.size, pygame.SRCALPHA)
+            pygame.draw.rect(surface, GRID_COLOR, rect, 1)
+    surface.blit(shape_surf, rect)
 
-			# compute 8-neighbor sum
-			# using toroidal boundary conditions - x and y wrap around
-			# so that the simulation takes place on a toroidal surface.
-			total = int((grid[i, (j-1)%N] + grid[i, (j+1)%N] +
-						grid[(i-1)%N, j] + grid[(i+1)%N, j] +
-						grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, (j+1)%N] +
-						grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N])/255)
-
-			# apply Conway's rules
-			if grid[i, j] == ON:
-				if (total < 2) or (total > 3):
-					newGrid[i, j] = OFF
-			else:
-				if total == 3:
-					newGrid[i, j] = ON
-
-	# update data
-	img.set_data(newGrid)
-	grid[:] = newGrid[:]
-	return img,
-
-# main() function
 def main():
+    """
+        Main entry point
+    """
+    grid = GOSPER_GLIDER
 
-	# Command line args are in sys.argv[1], sys.argv[2] ..
-	# sys.argv[0] is the script name itself and can be ignored
-	# parse arguments
-	parser = argparse.ArgumentParser(description="Runs Conway's Game of Life simulation.")
+    pygame.init()
+    screen = pygame.display.set_mode((600, 400))
 
-	# add arguments
-	parser.add_argument('--grid-size', dest='N', required=False)
-	parser.add_argument('--mov-file', dest='movfile', required=False)
-	parser.add_argument('--interval', dest='interval', required=False)
-	parser.add_argument('--glider', action='store_true', required=False)
-	parser.add_argument('--gosper', action='store_true', required=False)
-	args = parser.parse_args()
-	
-	# set grid size
-	N = 100
-	if args.N and int(args.N) > 8:
-		N = int(args.N)
-		
-	# set animation update interval
-	updateInterval = 50
-	if args.interval:
-		updateInterval = int(args.interval)
+    while True:
+        if pygame.QUIT in [e.type for e in pygame.event.get()]:
+            sys.exit(0)
 
-	# declare grid
-	grid = np.array([])
+        screen.fill(BACKGROUND_COLOR)
+        draw_grid(screen, grid)
+        make_squares(screen)
+        grid = update_grid(grid)
+        pygame.display.flip()
+        time.sleep(0.1)
 
-	# check if "glider" demo flag is specified
-	if args.glider:
-		grid = np.zeros(N*N).reshape(N, N)
-		addGlider(1, 1, grid)
-	elif args.gosper:
-		grid = np.zeros(N*N).reshape(N, N)
-		addGosperGliderGun(10, 10, grid)
 
-	else: # populate grid with random on/off -
-			# more off than on
-		grid = randomGrid(N)
-
-	# set up animation
-	fig, ax = plt.subplots(figsize=(5,5)) #figsize set size of figure. Like that it automatically opens with full windowed plot
-	img = ax.imshow(grid, interpolation='nearest')
-	ani = animation.FuncAnimation(fig, update, fargs=(img, grid, N, ),
-								frames = 30,
-								interval=updateInterval,
-								save_count=50)
-
-	# # of frames?
-	# set output file
-	if args.movfile:
-		ani.save(args.movfile, fps=30, extra_args=['-vcodec', 'libx264'])
-	# add grid from 0 to 100 and step = 1
-	major_ticks = np.arange(0, 100, 1)
-
-	ax.set_xticks(major_ticks)
-	ax.set_yticks(major_ticks)
-
-	ax.grid(which='minor', alpha=0.1)
-	ax.grid(which='major', alpha=0.1)
-  # remove lables from axis
-	plt.xticks(color='w')
-	plt.yticks(color='w')
-
-	plt.grid(True)
-  # move plot as it could be in centre of the window (without white frames around)
-	plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, hspace = 0, wspace = 0)
-	# plt.axis('off')
-	plt.show()
-
-if __name__ == '__main__':
-	main()
+if __name__ == "__main__":
+    main()
