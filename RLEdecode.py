b2444c16df56a5abeb3fd0ab3586b7ced2087419
Alisa <91307985+bouffee@users.noreply.github.com>
2023-11-10 21:56:17 +0300

    Update RLEdecode.py

--- RLEdecode.py
+++ RLEdecode.py
@@ -1,45 +1,39 @@
 # x = 36, y = 9
 # 24bo$22bobo$12b2o6b2o12b2o$11bo3bo4b2o12b2o$2o8bo5bo3b2o$2o8bo3bob2o4b
 # obo$10bo5bo7bo$11bo3bo$12b2o!
-from collections import namedtuple
-
-
 x_ans = 5
 y_ans = 5
 answer_coordinates = []
 
-def decodeRLE(message):
-    global x_ans, y_ans
-    for i in range(len(message)):
-        if message[i] == "$":
-            y_ans += 1
-            x_ans = 5
+def getSequences(line):
+    res = []
+    seq = ""
+    for el in line:
+        if el == "\n":
             continue
-        elif message[i].isdigit() and message[i+1].isdigit():
-            x_ans += int(message[i] + message[i+1])
-            continue
-        elif message[i].isdigit():
-            x_ans += int(message[i])
-            continue
-        elif message[i] == 'b' and message[i-1].isalpha():
-            x_ans += 1
-            continue
-        elif message[i] == 'b' and message[i-1].isdigit():
-            continue
-        elif message[i] == 'o':
-            if message[i-1].isdigit() and message[i-2].isdigit():
-                for j in range(x_ans, x_ans - int(message[i-2] + message[i-1]) + 1, -1):
-                    answer_coordinates.append((j, y_ans))
-                    continue
-            elif message[i-1].isdigit():
-                answer_coordinates.append((x_ans-int(message[i-1]), y_ans))
-                continue
-            elif message[i-1].isalpha():
-                x_ans += 1
-                answer_coordinates.append((x_ans, y_ans))
-                continue
+        seq += el
+        if el == "b" or el == "o":
+            if len(seq) == 1:
+                seq = "1" + seq
+            res.append(seq)
+            seq = ""
+    return res
 
-decodeRLE("5b$3bob$bobob$2b2ob$5b!") 
-with open("converted_pattern.txt", "w") as f:
-    for x, y in answer_coordinates:
-        f.write(f"{x},{y}\n")
+def decodeRLE(message):
+    lines = message.split("$")
+    x_ans = 5
+    y_ans = 5
+    for line in lines:
+        # print("#"*10)
+        # print(line)
+        sequences = getSequences(line)
+        for seq in sequences:
+            if seq[-1] == "o":
+                for i in range(int(seq[:-1])):
+                    answer_coordinates.append((x_ans + i, y_ans))
+                    # print((x_ans + i, y_ans))
+            x_ans += int(seq[:-1])
+        x_ans = 5
+        y_ans += 1
+    return answer_coordinates
+    answer_coordinates.clear()
