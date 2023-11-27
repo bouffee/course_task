c744951882b839b3cb76d32b93f647d0a3b576fa
Alisa <91307985+bouffee@users.noreply.github.com>
2023-11-27 23:27:33 +0300

    Update RLEdecode.py
    Fixed a bug when after second load of .rle-file grid was filled with old .rle-file grid

--- RLEdecode.py
+++ RLEdecode.py
@@ -1,10 +1,6 @@
 # x = 36, y = 9
 # 24bo$22bobo$12b2o6b2o12b2o$11bo3bo4b2o12b2o$2o8bo5bo3b2o$2o8bo3bob2o4b
 # obo$10bo5bo7bo$11bo3bo$12b2o!
-x_ans = 5
-y_ans = 5
-answer_coordinates = []
-
 def getSequences(line):
     res = []
     seq = ""
@@ -21,19 +17,19 @@ def getSequences(line):
 
 def decodeRLE(message):
     lines = message.split("$")
+    answer_coordinates = []
+    # print(answer_coordinates)
     x_ans = 5
     y_ans = 5
     for line in lines:
-        # print("#"*10)
-        # print(line)
         sequences = getSequences(line)
+        # print(" seq is :", sequences)
         for seq in sequences:
             if seq[-1] == "o":
                 for i in range(int(seq[:-1])):
+                    # print(x_ans + i, y_ans)
                     answer_coordinates.append((x_ans + i, y_ans))
-                    # print((x_ans + i, y_ans))
             x_ans += int(seq[:-1])
         x_ans = 5
         y_ans += 1
     return answer_coordinates
-    answer_coordinates.clear()
